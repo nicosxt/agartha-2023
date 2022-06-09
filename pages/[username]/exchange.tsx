7a4b26a94ed9299bf5f0ca1d7a7e05c1f6ed6bcc
Lucy Qiu <lucyqiu@Lucys-MacBook-Pro.local>
2022-06-09 15:49:28 -0500

    fix exchange bug

--- pages/[username]/exchange.tsx
+++ pages/[username]/exchange.tsx
@@ -95,7 +95,7 @@ export default function ExchangePage(props: User): any {
     </Link>
      <PostFeed posts={posts} admin={true} />
         <div className="mt-8 min-h-full">
-                {!loading && !postsEnd &&
+                {!loading && !postsEnd && posts.length > 0 && 
                 <button 
                     className=" w-full relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
 
