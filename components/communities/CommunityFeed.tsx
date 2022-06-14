f7d40ef832831fc0706340f0ae2f582431820d16
Lucy Qiu <lucyqiu@Lucys-Air.lan>
2022-06-14 13:12:15 -0500

    dark mode

--- components/communities/CommunityFeed.tsx
+++ components/communities/CommunityFeed.tsx
@@ -64,7 +64,7 @@ function Community(props: PostProps) : any {
                         <div className="truncate">
                         <div className="flex text-sm">
                             <a>
-                            <p className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{community.communityName}</p>
+                            <p className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{community.communityName}</p>
                             </a>
                         </div>
                         <div className="flex space-x-4">
