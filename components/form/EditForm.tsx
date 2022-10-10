52a88530decabeb90cc7c309b1143fda10490b87
Lucy Qiu <lucyqiu@Lucys-Air.lan>
2022-10-10 14:32:07 -0400

    remove

--- components/form/EditForm.tsx
+++ components/form/EditForm.tsx
@@ -130,7 +130,9 @@ export default function EditForm(props : any) {
                 <div className="space-y-6 sm:space-y-5">
                   <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
                     <label htmlFor="first-name" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> Availiability </label>
-                    <div date-rangepicker="true" className="flex items-center">
+                    <div className="flex items-center">
+
+                    {/* <div date-rangepicker="true" className="flex items-center"> */}
                       <div className="relative">
                             <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                                 <svg className="w-5 h-5 text-gray-500 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd"></path></svg>
