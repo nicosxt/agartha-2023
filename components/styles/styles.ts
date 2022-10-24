29d336d92ace8b749d3b5ba0d6704aa0a0070438
Lucy Qiu <lucy1049684100@gmail.com>
2022-10-23 23:54:03 -0400

    fix particles

--- components/styles/styles.ts
+++ components/styles/styles.ts
@@ -1,20 +1,15 @@
 import styled from 'styled-components';
 
 const Banner = styled.div`
-    position: fixed;
-    z-index: 100;
+
 `;
 
 const Body = styled.div`
-position: fixed
-z-index: 100;
+
 `;
 
 const Container = styled.div`
-position: fixed;
-width: 50%;
-height: 50%;
-z-index: 100;
+
 `;
 
 export  {Banner, Container, Body};
