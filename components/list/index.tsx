464ac8131e120be9b568edf8daaef61bad91105e
Lucy Qiu <lucy1049684100@gmail.com>
2022-10-24 21:58:29 -0400

    list padding

--- components/list/index.tsx
+++ components/list/index.tsx
@@ -25,17 +25,15 @@ import { useEffect, useState } from "react";
 
 
     return (
-      <div className="min-h-screen bg-white ">
-        <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-full lg:px-0">
-          <h1 className="text-4xl text-center pb-4 text-transparent bg-clip-text bg-gradient-to-r from-[#47FF43] via-[#6184FF] to-[#FF43EC] font-extrabold sm:text-4xl md:text-4xl">
-            Explore Meaningful Communities
-            </h1>
+      <div className="bg-white ">
+        <div className=" py-16 px-4 sm:py-24 sm:px-6 lg:max-w-full lg:px-0">
 
-          <div className="-mx-px mt-6 grid grid-cols-1 gap-8 sm:grid-cols-2 mx-4 lg:mr-32 lg:grid-cols-4 ">
+
+          <div className="mt-6 grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-col-3 lg:mr-32 lg:grid-cols-4 ">
 
             {communities && communities.map((community:any) => (
-              <div key={community.slug} className="group relative">
-                <div className="border-2 border-[#0000FF] bg-[#EAFFF4] lg:h-96 lg:w-72 w-72 h-96  mx-20 bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80 bg-[#DDFFF3]">
+              <div key={community.slug} className=" ">
+                <div className="mx-10 border-2  border-[#0000FF] bg-[#EAFFF4] lg:h-96 lg:w-72 w-72 h-96   bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80 bg-[#DDFFF3]">
                     <div className="h-60 w-60  mt-6 mx-5 lg:mx-5 mb-2 border-2 border-[#0000FF] ">
                   <img
                     src={community.avatarUrl}
@@ -45,7 +43,7 @@ import { useEffect, useState } from "react";
                   <div>
                     <h3 className="text-md mt-4 font-bold font-inter text-[#0000FF] text-center">
                       <a href={`/community/${community.slug}`}>
-                        <span aria-hidden="true" className="absolute  inset-0" />
+                        <span aria-hidden="true" className="" />
                         {community.communityName}
                       </a>
                     </h3>
