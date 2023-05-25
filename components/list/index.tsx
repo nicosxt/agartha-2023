22e96ce77a3944dcd30323ccfabf0f4171d48f93
wills <will@simons.dev>
2023-05-24 19:36:07 -0700

    use only tags defined in DB

--- components/list/index.tsx
+++ components/list/index.tsx
@@ -1,5 +1,5 @@
 import Fuse from "fuse.js";
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 
 import style from "./list.module.css";
 import CommunityProfilePage from "../communities/CommunityProfilePage";
@@ -47,18 +47,18 @@ export default function CommunityList(props: any) {
   const [communityQuery, setCommunityQuery] = useState("");
   const [communityView, setCommunityView] = useState(null);
 
-  const tags = [
-    "Regen",
-    "Techy",
-    "Artsy",
-    "DAO",
-    "Coliving",
-    "Earthship",
-    "Farm",
-    "Solarpunk",
-    "Family",
-    "Buddhism",
-  ];
+  const tags = useMemo(
+    () =>
+      Object.keys(
+        communities.reduce((tagAcc: any, c: any) => {
+          c.tags.forEach((tag: any) => {
+            tagAcc[tag] = tagAcc[tag] ? tagAcc[tag] + 1 : 0;
+          });
+          return tagAcc;
+        }, {})
+      ),
+    [communities]
+  );
 
   const fuse = new Fuse(communities, {
     keys: ["name", "description", "tags", "country"],
@@ -92,66 +92,68 @@ export default function CommunityList(props: any) {
   }
 
   return (
-    <div className="bg-white ">
-      <div className="max-w-2xl mx-auto py-12 px-4 sm:px-6 md:px-8 lg:max-w-7xl lg:px-8">
-        <form>
-          <label
-            htmlFor="default-search"
-            className="mb-2 text-sm font-medium text-gray-900 sr-only "
-          >
-            Search
-          </label>
-          <div className="relative">
-            <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-              <svg
-                aria-hidden="true"
-                className="w-5 h-5 text-[#0000FF] "
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="3"
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                ></path>
-              </svg>
-            </div>
-            <input
-              type="search"
-              value={communityQuery}
-              onChange={handleOnSearch}
-              id="default-search"
-              className="block p-4 pl-10 w-full text-lg text-gray-900 bg-[#FFDDED] border border-2 border-[#0000FF] focus:ring-blue-500 focus:border-blue-500 placeholder:text-[#0000FF] placeholder:italic"
-              placeholder="Search Keywords..."
-              required
-            />
-          </div>
-        </form>
-        <div className="flex flex-wrap mt-8 mx-8 lg:mt-1 lg:ml-4 md:mx-5 md:mt-2 ">
-          {tags.map((tag: any) => (
-            <button
-              className="mt-2 hover:scale-125 text-[#FFDDED] bg-[#0000FF] rounded-3xl py-1 px-4 font-mono font-medium lg:text-xl mr-2"
-              onClick={() => changeQuery(tag)}
-              key={tag}
+    <div className="bg-white">
+      <div className={`${communityView ? style.communityView : ''}`}>
+        <div className="max-w-2xl mx-auto py-12 px-4 sm:px-6 md:px-8 lg:max-w-7xl lg:px-8">
+          <form>
+            <label
+              htmlFor="default-search"
+              className="mb-2 text-sm font-medium text-gray-900 sr-only "
             >
-              #{tag}
-            </button>
-          ))}
-        </div>
-      </div>
-      <div className=" py-16 px-4 sm:py-2 sm:px-6 md:py-2 lg:max-w-full lg:px-0 lg:py-2 xl:py-2">
-        <div className={style.cardGrid}>
-          {searchResults &&
-            searchResults.map((community: any) => (
-              <Card
-                key={community.slug}
-                community={community}
-                onClick={onClickCommunity}
+              Search
+            </label>
+            <div className="relative">
+              <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                <svg
+                  aria-hidden="true"
+                  className="w-5 h-5 text-[#0000FF] "
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="3"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  ></path>
+                </svg>
+              </div>
+              <input
+                type="search"
+                value={communityQuery}
+                onChange={handleOnSearch}
+                id="default-search"
+                className="block p-4 pl-10 w-full text-lg text-gray-900 bg-[#FFDDED] border border-2 border-[#0000FF] focus:ring-blue-500 focus:border-blue-500 placeholder:text-[#0000FF] placeholder:italic"
+                placeholder="Search Keywords..."
+                required
               />
+            </div>
+          </form>
+          <div className="flex flex-wrap mt-8 mx-8 lg:mt-1 lg:ml-4 md:mx-5 md:mt-2 ">
+            {tags.map((tag: any) => (
+              <button
+                className="mt-2 hover:scale-125 text-[#FFDDED] bg-[#0000FF] rounded-3xl py-1 px-4 font-mono font-medium lg:text-xl mr-2"
+                onClick={() => changeQuery(tag)}
+                key={tag}
+              >
+                #{tag}
+              </button>
             ))}
+          </div>
+        </div>
+        <div className=" py-16 px-4 sm:py-2 sm:px-6 md:py-2 lg:max-w-full lg:px-0 lg:py-2 xl:py-2">
+          <div className={style.cardGrid}>
+            {searchResults &&
+              searchResults.map((community: any) => (
+                <Card
+                  key={community.slug}
+                  community={community}
+                  onClick={onClickCommunity}
+                />
+              ))}
+          </div>
         </div>
       </div>
       {!!communityView ? (
