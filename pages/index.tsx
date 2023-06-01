004bdb5171272ee2590ed278552e5a681011f95d
wills <will@simons.dev>
2023-06-01 13:32:23 -0700

    enter button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,27 @@
 import { useCallback, useState } from "react";
 import Map, { getServerSideProps as mapGetServerSideProps } from "./map";
-import styles from '../components/map/landing.module.css';
+import styles from "../components/map/landing.module.css";
 
 export default function Index(props) {
-  const [landing, setLanding] = useState(false)
+  const [landing, setLanding] = useState(false);
   const toMap = useCallback(() => {
     setLanding(!landing);
-  }, [landing])
+  }, [landing]);
   return (
     <div>
       <Map communities={props.communities} isBackground={!landing} />
-      <div className={`${styles.landingWrapper} ${!landing ? '' : styles.fadeOut}`}>
-        <h2 className={styles.landingHeader}>Solarpunk Communities Intersecting Art, Ecology and Technology</h2>
-        <button onClick={toMap}>Enter</button>
+      <div
+        className={`${styles.landingWrapper} ${!landing ? "" : styles.fadeOut}`}
+      >
+        <h2 className={styles.landingHeader}>
+          Solarpunk Communities Intersecting Art, Ecology and Technology
+        </h2>
+        <div className={styles.enterButtonWrapper}>
+          <div className={styles.enterButtonBackground}></div>
+          <button className={styles.enterButton} onClick={toMap}>
+            Enter
+          </button>
+        </div>
       </div>
     </div>
   );
